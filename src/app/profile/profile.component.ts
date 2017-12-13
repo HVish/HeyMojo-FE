a4d3f76c4e8da9eea3e53c68e291804c3ceb3dca
Vishnu Singh <vishnu@localhost.localdomain>
2017-12-13 17:42:47 +0530

    profiile layout

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { SessionService } from '../session.service';
+
+import { User } from '../user';
+import { Organization } from '../organization';
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+    selector: 'app-profile',
+    templateUrl: './profile.component.html',
+    styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+    user: User;
 
-  constructor() { }
+    constructor(private sessionService: SessionService) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.user = this.sessionService.getItem('user', {});
+    }
+
+    formatDate(date: any): string {
+        const d: Date = new Date(date);
+        return d.toLocaleDateString('en-US', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric'
+        });
+    }
+
+    formatOrg(org: Organization): string {
+        const start = this.formatDate(org.start);
+        const end = org.end ? this.formatDate(org.end) : 'present';
+        return `${org.name} [ ${start} - ${end} ]`;
+    }
 
 }
